214f2a66fdbfd3ce463503b1e9b43bff8cecc38d
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-27 15:07:01 -0500

    first commit

---
+++ src/main/java/com/ulysseswomen/shedigscode/SheDigsCodeApplication.java
@@ -0,0 +1,12 @@
+package com.ulysseswomen.shedigscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheDigsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheDigsCodeApplication.class, args);
+	}
+}
