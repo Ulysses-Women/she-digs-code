ab204f00b991d11d84d35871db17125788b10734
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-27 15:41:05 -0500

    built controller

---
+++ src/main/java/com/ulysseswomen/shedigscode/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.ulysseswomen.shedigscode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MainController {
+
+    @GetMapping("/profiles")
+    public String index(){
+        return "profiles";
+    }
+}
