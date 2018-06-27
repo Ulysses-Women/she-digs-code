c84fe9b6328ebc5072e70b855e0668e0d54cba12
Jennifer Walker <dataqueen16@gmail.com>
2018-06-27 16:36:10 -0500

    1st commit

--- src/main/java/com/ulysseswomen/shedigscode/controller/MainController.java
+++ src/main/java/com/ulysseswomen/shedigscode/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+
     @GetMapping("/profiles")
     public String index(){
         return "profiles";
